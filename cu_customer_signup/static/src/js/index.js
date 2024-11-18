bb283553f674e1e76ba5c3e1a76d562b8b796fe3
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2024-11-18 13:22:17 -0400

    Added js to multistep form

---
+++ cu_customer_signup/static/src/js/index.js
@@ -0,0 +1,26 @@
+odoo.define('auth_signup.signup', function (require) {
+    "use strict";
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const steps = document.querySelectorAll('.step');
+        const nextButtons = document.querySelectorAll('.next-button');
+        const prevButtons = document.querySelectorAll('.prev-button');
+
+        nextButtons.forEach((button) => {
+            button.addEventListener('click', () => {
+                const currentStep = document.querySelector(`#${button.dataset.next}-indicator`);
+                const nextStep = document.querySelector(`#${button.dataset.next}`);
+                steps.forEach((step) => step.classList.add('d-none'));
+                nextStep.classList.remove('d-none');
+            });
+        });
+
+        prevButtons.forEach((button) => {
+            button.addEventListener('click', () => {
+                const prevStep = document.querySelector(`#${button.dataset.prev}`);
+                steps.forEach((step) => step.classList.add('d-none'));
+                prevStep.classList.remove('d-none');
+            });
+        });
+    });
+});
