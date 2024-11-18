bc520e4ff77f45c7f3d98798e645ddea8407bb6f
Aditya (Ruben) Prasad <adityaprasad7123@gmail.com>
2024-11-18 13:54:50 -0400

    Made deps array empty

--- cu_customer_signup/static/src/js/index.js
+++ cu_customer_signup/static/src/js/index.js
@@ -1,40 +1,27 @@
-odoo.define('auth_signup.signup', [
-    'web.public.widget'
-], function (require) {
+
+odoo.define('auth_signup.signup', [], function (require) {
     "use strict";
 
-    const publicWidget = require('web.public.widget');
+    document.addEventListener('DOMContentLoaded', () => {
+        const steps = document.querySelectorAll('.step');
+        const nextButtons = document.querySelectorAll('.next-button');
+        const prevButtons = document.querySelectorAll('.prev-button');
 
-    publicWidget.registry.AuthSignup = publicWidget.Widget.extend({
-        selector: '.oe_signup_form', // Attach to the signup form element
-        events: {
-            'click .next-button': '_onNextButtonClick',
-            'click .prev-button': '_onPrevButtonClick',
-        },
-        start: function () {
-            this.steps = this.$('.step');
-            this.currentStepIndex = 0;
-            this._showStep(this.currentStepIndex);
-            return this._super.apply(this, arguments);
-        },
-        _onNextButtonClick: function (ev) {
-            ev.preventDefault();
-            if (this.currentStepIndex < this.steps.length - 1) {
-                this.currentStepIndex++;
-                this._showStep(this.currentStepIndex);
-            }
-        },
-        _onPrevButtonClick: function (ev) {
-            ev.preventDefault();
-            if (this.currentStepIndex > 0) {
-                this.currentStepIndex--;
-                this._showStep(this.currentStepIndex);
-            }
-        },
-        _showStep: function (index) {
-            this.steps.addClass('d-none');
-            this.steps.eq(index).removeClass('d-none');
-        },
+        nextButtons.forEach((button) => {
+            button.addEventListener('click', () => {
+                const currentStep = document.querySelector(`#${button.dataset.next}-indicator`);
+                const nextStep = document.querySelector(`#${button.dataset.next}`);
+                steps.forEach((step) => step.classList.add('d-none'));
+                nextStep.classList.remove('d-none');
+            });
+        });
+
+        prevButtons.forEach((button) => {
+            button.addEventListener('click', () => {
+                const prevStep = document.querySelector(`#${button.dataset.prev}`);
+                steps.forEach((step) => step.classList.add('d-none'));
+                prevStep.classList.remove('d-none');
+            });
+        });
     });
-
 });
